d4a7f83cfd947a67395a7cf0b10d2bab12e7a37b
adam.bryksy <adam.bryksy@ramsat.pl>
2017-11-08 12:56:09 +0100

    Refaktoryzacja kodu -> przeniesienie klasy Pair na zewnątrz

---
+++ src/api/Pair.java
@@ -0,0 +1,37 @@
+package api;
+
+/**
+ *
+ * @author Adam
+ */
+public class Pair {
+    String first;
+    String second;
+
+    public Pair(String first, String second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" + "first=" + first + ", second=" + second + '}';
+    }
+
+}
--- src/streams/Streams.java
+++ src/streams/Streams.java
@@ -1,42 +1,12 @@
 package streams;
 
+import api.Pair;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-class Pair {
-        String first;
-        String second;
-        
-        public Pair(String first, String second) {
-            this.first = first;
-            this.second = second;
-        }
 
-    public String getFirst() {
-        return first;
-    }
-
-    public void setFirst(String first) {
-        this.first = first;
-    }
-
-    public String getSecond() {
-        return second;
-    }
-
-    public void setSecond(String second) {
-        this.second = second;
-    }
-
-    @Override
-    public String toString() {
-        return "Pair{" + "first=" + first + ", second=" + second + '}';
-    }
-        
-    
-}
 
 public class Streams {
     
